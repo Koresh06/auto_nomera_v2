06a26997a2a1928396c6fbb2e3369c2750fdc2db
Koresh06 <korets-24@mail.ru>
2026-07-07 00:49:14 +0300

    del default bot_proxy

--- src/core/config/telegram.py
+++ src/core/config/telegram.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 class TelegramSettings(BaseSettings):
     bot_token: str = "your_bot_token"
-    bot_proxy: str = "socks5://127.0.0.1:1080"
+    bot_proxy: str = ""
     admin_ids: list[int] = [123456789]
     bot_url: str = "https://t.me/your_bot"
     buyout_url: str = "https://t.me/your_buyout_bot"
