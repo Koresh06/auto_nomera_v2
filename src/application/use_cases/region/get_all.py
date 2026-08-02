22877388018b920f4cc41b42ae9ff145d010e8a5
Koresh06 <korets-24@mail.ru>
2026-08-02 22:15:04 +0300

    add sorting regions

--- src/application/use_cases/region/get_all.py
+++ src/application/use_cases/region/get_all.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from src.application.dtos.region import RegionDTO
 from src.application.ports.region.region_repo import RegionRepository
 from src.application.use_cases.base import UseCase, UseCaseRequest
+from src.domain.services.region.sorting import sort_regions
 
 
 @dataclass(frozen=True, eq=False)
@@ -16,4 +17,5 @@ class GetAllRegionsUseCase(UseCase[GetRegionsRequest, list[RegionDTO]]):
 
     async def __call__(self, command: GetRegionsRequest) -> list[RegionDTO]:
         regions = await self.region_repo.get_all()
-        return [RegionDTO.from_entity(region) for region in regions]
+        dtos = [RegionDTO.from_entity(r) for r in regions]
+        return sort_regions(dtos)
---
+++ src/domain/services/region/sorting.py
@@ -0,0 +1,32 @@
+import re
+
+from src.application.dtos.region import RegionDTO
+
+MOSCOW_CODES = {77, 97, 99, 50, 90}
+SPB_CODES = {78, 98, 47}
+
+
+def extract_region_codes(region: RegionDTO) -> list[int]:
+    codes: list[str] = []
+    if region.channel_username:
+        codes += re.findall(r"\d+", region.channel_username)
+    if region.title:
+        codes += re.findall(r"\|(\d+)\|", region.title)
+    return [int(c) for c in codes]
+
+
+def region_sort_key(region: RegionDTO) -> tuple[int, int]:
+    codes = extract_region_codes(region)
+    codes_set = set(codes)
+
+    if codes_set & MOSCOW_CODES:
+        return (0, 0)
+    if codes_set & SPB_CODES:
+        return (1, 0)
+    if codes:
+        return (2, min(codes))
+    return (3, 999)
+
+
+def sort_regions(regions: list[RegionDTO]) -> list[RegionDTO]:
+    return sorted(regions, key=region_sort_key)
